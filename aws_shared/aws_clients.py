a38edc257c8312842b75807381ef19cdb2c8b17a
cylb0 <foucaut.morgan@gmail.com>
2026-04-29 13:02:56 +0200

    refactor: standardize AWS configuration and add default bucket/queue support to AWS client methods

--- aws_shared/aws_clients.py
+++ aws_shared/aws_clients.py
@@ -7,20 +7,36 @@ class AWSClientManager:
     def __init__(self):
         profile = os.getenv("AWS_PROFILE")
         region = os.getenv("AWS_REGION_NAME")
+
         self.session = (
             boto3.Session(profile_name=profile, region_name=region)
             if profile
             else boto3.Session(region_name=region)
         )
+        self.default_bucket = os.getenv("AWS_STORAGE_BUCKET")
+        self.default_queue_url = os.getenv("AWS_QUEUE_URL")
+
         self.s3 = self.session.client("s3")
         self.sqs = self.session.client("sqs")
 
-    def upload_file(self, local_path, bucket, s3_key):
-        return self.s3.upload_file(local_path, bucket, s3_key)
+    def upload_file(self, local_path, s3_key, bucket_name=None):
+        target_bucket = bucket_name or self.default_bucket
+        if not target_bucket:
+            raise ValueError("AWS_STORAGE_BUCKET environment variable not set")
+        return self.s3.upload_file(local_path, target_bucket, s3_key)
 
-    def send_task(self, queue_url, task: ImageTask):
+    def download_file(self, s3_key, local_path, bucket_name=None):
+        target_bucket = bucket_name or self.default_bucket
+        if not target_bucket:
+            raise ValueError("AWS_STORAGE_BUCKET environment variable not set")
+        return self.s3.download_file(target_bucket, s3_key, local_path)
+
+    def send_task(self, task: ImageTask, queue_url=None):
+        target_queue_url = queue_url or self.default_queue_url
+        if not target_queue_url:
+            raise ValueError("AWS_QUEUE_URL environment variable not set")
         return self.sqs.send_message(
-            QueueUrl=queue_url, MessageBody=task.model_dump_json()
+            QueueUrl=target_queue_url, MessageBody=task.model_dump_json()
         )
 
 
