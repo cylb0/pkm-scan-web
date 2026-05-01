fc16274c9ace4d1a40c0808bb3e73ed58af7e4d6
cylb0 <foucaut.morgan@gmail.com>
2026-05-01 11:35:39 +0200

    refactor(aws): implement Enums and strict typing for SQS/S3 resources

--- services/img_processor/tasks.py
+++ services/img_processor/tasks.py
@@ -2,7 +2,7 @@ import os
 import logging
 from aws_shared.schemas import ImageTask
 from aws_shared.constants import FOLDER_MEDIA, get_s3_img_key
-from aws_shared.aws_clients import AWSClientManager
+from aws_shared.aws_clients import AWSClientManager, QueueAlias
 from errors.processing import PokemonCardDetectionError
 from processor import crop_card_border
 
@@ -50,7 +50,7 @@ def process_messages(messages, client):
             task = ImageTask.model_validate_json(msg["Body"])
             logger.info(f"Processing image for card {task.card_id}")
             process_image(task, client)
-            client.delete_message(msg["ReceiptHandle"])
+            client.delete_message(msg["ReceiptHandle"], QueueAlias.RAW_IMAGES)
             logger.info(f"Image processed and deleted for card {task.card_id}")
         except Exception as e:
             logger.error(f"Failed to process {msg['MessageId']}: {e}")
